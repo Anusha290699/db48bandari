8d9ce0249c76305bf9ed962f9b5b90b4a51a69dd
Anusha290699 <89500744+Anusha290699@users.noreply.github.com>
2021-11-15 19:35:44 -0600

    updated

--- controllers/jacket.js
+++ controllers/jacket.js
@@ -25,9 +25,17 @@ exports.jacket_view_all_Page = async function(req, res) {
 }; 
 
 // for a specific jacket.
-exports.jacket_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: jacket detail: ' + req.params.id); 
-};
+// for a specific jacket. 
+exports.jacket_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await jacket.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
 // Handle jacket create on POST.
 exports.jacket_create_post = async function (req, res) {
     console.log(req.body)
@@ -53,7 +61,33 @@ exports.jacket_delete = function(req, res) {
     res.send('NOT IMPLEMENTED: jacket delete DELETE ' + req.params.id); 
 }; 
  
+exports.jacket_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await jacket.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+}; 
+ 
 // Handle jacket update form on PUT. 
-exports.jacket_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: jacket  update PUT' + req.params.id); 
-};
+exports.jacket_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await jacket.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.name)  toUpdate.name= req.body.name; 
+        if(req.body.brand) toUpdate.brand = req.body.brand; 
+        if(req.body.price) toUpdate.price = req.body.price; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
