6357b8b08e9dc715f657791664b204c45f3cfdbc
Anusha290699 <89500744+Anusha290699@users.noreply.github.com>
2021-11-22 18:38:34 -0600

    added validation

--- models/jacket.js
+++ models/jacket.js
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose") 
 const jacketSchema = mongoose.Schema({
     
-    
-        name: String,
-        brand:  String,
-        price: Number
-}) 
+    name:{
+    type: String,
+    minLength: 3,
+    maxLength: 50
+},
+
+brand : String,
+
+price: {
+    type:Number,
+    min:1,
+    max:200
+}
+})
  
 module.exports = mongoose.model("jacket", jacketSchema)
--- routes/jacket.js
+++ routes/jacket.js
@@ -2,6 +2,17 @@ var express = require("express");
 const jacket_controlers= require('../controllers/jacket');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 /* GET home page. */
 router.get('/', jacket_controlers.jacket_view_all_Page);
 
@@ -12,7 +23,7 @@ router.get('/detail', jacket_controlers.jacket_view_one_Page);
 router.get('/create', jacket_controlers.jacket_create_Page); 
 
 /* GET create jacket page */ 
-router.get('/update', jacket_controlers.jacket_update_Page);
+router.get('/update',secured, jacket_controlers.jacket_update_Page);
 
 /* GET create jacket page */ 
 router.get('/delete', jacket_controlers.jacket_delete_Page);
